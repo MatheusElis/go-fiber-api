64c3fcdc7287001d6912643d955898e22da1494b
Matheus Elis <matheus.elis@2rpnet.com>
2024-09-11 16:58:25 -0300

    feat: include client base app

---
+++ react-go-tutorial/client/src/App.tsx
@@ -0,0 +1,17 @@
+import { Container, Stack } from '@chakra-ui/react'
+import Navbar from './components/Navbar'
+import TodoForm from './components/TodoForm'
+
+function App() {
+
+  return (
+    <Stack h="100vh">
+      <Navbar />
+      <Container>
+        <TodoForm />
+      </Container>
+    </Stack>
+  )
+}
+
+export default App
